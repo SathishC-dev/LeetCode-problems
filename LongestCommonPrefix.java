31dc72bc4f55cd54bfe184a64dce050e86aa8a3f
sathish <sathish.c@gkdigitalhut.com>
2026-02-24 20:32:44 +0530

    Longest Common Prefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        String output = "";
+        for (int i = 0;i<s1.length();i++){
+            if(s1.charAt(i) == s2.charAt(i))
+                output = output+s1.charAt(i);
+            
+            else
+                break;
+
+        }return output;
+    }
+}
